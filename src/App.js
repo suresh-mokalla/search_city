fe862d31e6b567dd243d8d5f67f4766deefd2c34
suresh <lovelysuresh2277@gmail.com>
2024-11-05 12:11:50 +0530

    searchcities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Data from './city.json';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Enter City Names :</h3>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <br />
+      {Data.filter((city) =>
+        city.name.toLowerCase().includes(search.toLowerCase())
+      ).map((city) => {
+        return (
+          <div
+            style={{
+              border: '1px solid black',
+              margin: '10px',
+              padding: '10px',
+              maxwidth: '70%',
+            }}
+          >
+            {city.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
